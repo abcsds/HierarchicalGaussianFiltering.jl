403310b4bfaeead03034c33f990c67c5e1f94996
Peter Thestrup Waade <peter@waade.net>
2025-06-20 16:31:12 +0200

    for now just returning deepcopy of the HGF struct

--- src/ActionModels_extensions/initialize_attributes.jl
+++ src/ActionModels_extensions/initialize_attributes.jl
@@ -1,7 +1,10 @@
 function ActionModels.initialize_attributes(
-    submodel::MyRescorlaWagner,
+    submodel::HGF,
     ::Type{TF} = Float64,
     ::Type{TI} = Int64,
 ) where {TF,TI}
+
+    #For now, we don't initialize any attributes
+    return deepcopy(submodel)
 
 end
