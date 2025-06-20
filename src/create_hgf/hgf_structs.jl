eeb8d820e9a9b64bc01e2d1d60a1cbdc8ddb0928
Peter Thestrup Waade <peter@waade.net>
2025-06-20 16:28:23 +0200

    undone the typing for now

--- src/create_hgf/hgf_structs.jl
+++ src/create_hgf/hgf_structs.jl
@@ -58,21 +58,21 @@ abstract type PrecisionCoupling <: CouplingType end
 
 #Concrete value coupling types
 Base.@kwdef mutable struct DriftCoupling <: ValueCoupling
-    strength::Union{Nothing,Float64} = nothing
+    strength::Union{Nothing,Real} = nothing
     transform::CouplingTransform = LinearTransform()
 end
 Base.@kwdef mutable struct ProbabilityCoupling <: ValueCoupling
-    strength::Union{Nothing,Float64} = nothing
+    strength::Union{Nothing,Real} = nothing
 end
 Base.@kwdef mutable struct CategoryCoupling <: ValueCoupling end
 Base.@kwdef mutable struct ObservationCoupling <: ValueCoupling end
 
 #Concrete precision coupling types
 Base.@kwdef mutable struct VolatilityCoupling <: PrecisionCoupling
-    strength::Union{Nothing,Float64} = nothing
+    strength::Union{Nothing,Real} = nothing
 end
 Base.@kwdef mutable struct NoiseCoupling <: PrecisionCoupling
-    strength::Union{Nothing,Float64} = nothing
+    strength::Union{Nothing,Real} = nothing
 end
 
 ############################
@@ -90,7 +90,7 @@ end
 
 """
 """
-Base.@kwdef mutable struct HGF <: ActionModels.AbstractSubmodelAttributes
+Base.@kwdef mutable struct HGF
     all_nodes::Dict{String,AbstractNode}
     input_nodes::Dict{String,AbstractInputNode}
     state_nodes::Dict{String,AbstractStateNode}
@@ -104,30 +104,30 @@ end
 ######## HGF Info Structs ########
 ##################################
 Base.@kwdef struct NodeDefaults
-    input_noise::Float64 = -2
-    bias::Float64 = 0
-    volatility::Float64 = -2
-    drift::Float64 = 0
-    autoconnection_strength::Float64 = 1
-    initial_mean::Float64 = 0
-    initial_precision::Float64 = 1
-    coupling_strength::Float64 = 1
+    input_noise::Real = -2
+    bias::Real = 0
+    volatility::Real = -2
+    drift::Real = 0
+    autoconnection_strength::Real = 1
+    initial_mean::Real = 0
+    initial_precision::Real = 1
+    coupling_strength::Real = 1
     update_type::HGFUpdateType = EnhancedUpdate()
 end
 
 Base.@kwdef mutable struct ContinuousState <: AbstractStateNodeInfo
     name::String
-    volatility::Union{Float64,Nothing} = nothing
-    drift::Union{Float64,Nothing} = nothing
-    autoconnection_strength::Union{Float64,Nothing} = nothing
-    initial_mean::Union{Float64,Nothing} = nothing
-    initial_precision::Union{Float64,Nothing} = nothing
+    volatility::Union{Real,Nothing} = nothing
+    drift::Union{Real,Nothing} = nothing
+    autoconnection_strength::Union{Real,Nothing} = nothing
+    initial_mean::Union{Real,Nothing} = nothing
+    initial_precision::Union{Real,Nothing} = nothing
 end
 
 Base.@kwdef mutable struct ContinuousInput <: AbstractInputNodeInfo
     name::String
-    input_noise::Union{Float64,Nothing} = nothing
-    bias::Union{Float64,Nothing} = nothing
+    input_noise::Union{Real,Nothing} = nothing
+    bias::Union{Real,Nothing} = nothing
 end
 
 Base.@kwdef mutable struct BinaryState <: AbstractStateNodeInfo
@@ -170,40 +170,40 @@ end
 """
 Configuration of continuous state nodes' parameters 
 """
-Base.@kwdef mutable struct ContinuousStateNodeParameters{T<:Real}
-    volatility::T = 0
-    drift::T = 0
-    autoconnection_strength::T = 1
-    initial_mean::T = 0
-    initial_precision::T = 0
-    coupling_strengths::Dict{String,T} = Dict{String,Float64}()
-    coupling_transforms::Dict{String,CouplingTransform} = Dict{String,CouplingTransform}()
+Base.@kwdef mutable struct ContinuousStateNodeParameters
+    volatility::Real = 0
+    drift::Real = 0
+    autoconnection_strength::Real = 1
+    initial_mean::Real = 0
+    initial_precision::Real = 0
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
+    coupling_transforms::Dict{String,CouplingTransform} = Dict{String,Real}()
 end
 
 """
 Configurations of the continuous state node states
 """
-Base.@kwdef mutable struct ContinuousStateNodeState{T<:Real}
-    posterior_mean::Union{T} = 0
-    posterior_precision::Union{T} = 1
-    value_prediction_error::Union{T,Missing} = missing
-    precision_prediction_error::Union{T,Missing} = missing
-    prediction_mean::Union{T,Missing} = missing
-    prediction_precision::Union{T,Missing} = missing
-    effective_prediction_precision::Union{T,Missing} = missing
+Base.@kwdef mutable struct ContinuousStateNodeState
+    posterior_mean::Union{Real} = 0
+    posterior_precision::Union{Real} = 1
+    value_prediction_error::Union{Real,Missing} = missing
+    precision_prediction_error::Union{Real,Missing} = missing
+    prediction_mean::Union{Real,Missing} = missing
+    prediction_precision::Union{Real,Missing} = missing
+    effective_prediction_precision::Union{Real,Missing} = missing
 end
 
 """
 Configuration of continuous state node history
 """
-Base.@kwdef mutable struct ContinuousStateNodeHistory{T<:Real}
-    posterior_mean::Vector{Union{T,Missing}} = []
-    posterior_precision::Vector{Union{T,Missing}} = []
-    value_prediction_error::Vector{Union{T,Missing}} = []
-    precision_prediction_error::Vector{Union{T,Missing}} = []
-    prediction_mean::Vector{Union{T,Missing}} = []
-    prediction_precision::Vector{Union{T,Missing}} = []
-    effective_prediction_precision::Vector{Union{T,Missing}} = []
+Base.@kwdef mutable struct ContinuousStateNodeHistory
+    posterior_mean::Vector{Real} = []
+    posterior_precision::Vector{Real} = []
+    value_prediction_error::Vector{Union{Real,Missing}} = []
+    precision_prediction_error::Vector{Union{Real,Missing}} = []
+    prediction_mean::Vector{Union{Real,Missing}} = []
+    prediction_precision::Vector{Union{Real,Missing}} = []
+    effective_prediction_precision::Vector{Union{Real,Missing}} = []
 end
 
 """
@@ -233,36 +233,37 @@ end
 """
 Configuration of continuous input node parameters
 """
-Base.@kwdef mutable struct ContinuousInputNodeParameters{T<:Real}
-    input_noise::Union{T,Nothing} = nothing
-    bias::Union{T,Nothing} = nothing
-    coupling_strengths::Dict{String,Union{T,Nothing}} =
-        Dict{String,Union{Float64,Nothing}}()
-    coupling_transforms::Dict{String,CouplingTransform} = Dict{String,CouplingTransform}()
+Base.@kwdef mutable struct ContinuousInputNodeParameters
+    input_noise::Real = 0
+    bias::Real = 0
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
+    coupling_transforms::Dict{String,CouplingTransform} = Dict{String,Real}()
 end
 
 """
 Configuration of continuous input node states
 """
-Base.@kwdef mutable struct ContinuousInputNodeState{T<:Real}
-    input_value::Union{T,Missing} = missing
-    value_prediction_error::Union{T,Missing} = missing
-    precision_prediction_error::Union{T,Missing} = missing
-    prediction_mean::Union{T,Missing} = missing
-    prediction_precision::Union{T,Missing} = missing
+Base.@kwdef mutable struct ContinuousInputNodeState
+    input_value::Union{Real,Missing} = missing
+    value_prediction_error::Union{Real,Missing} = missing
+    precision_prediction_error::Union{Real,Missing} = missing
+    prediction_mean::Union{Real,Missing} = missing
+    prediction_precision::Union{Real,Missing} = missing
 end
 
 """
 Configuration of continuous input node history
 """
-Base.@kwdef mutable struct ContinuousInputNodeHistory{T<:Real}
-    input_value::Vector{Union{T,Missing}} = []
-    value_prediction_error::Vector{Union{T,Missing}} = []
-    precision_prediction_error::Vector{Union{T,Missing}} = []
-    prediction_mean::Vector{Union{T,Missing}} = []
-    prediction_precision::Vector{Union{T,Missing}} = []
+Base.@kwdef mutable struct ContinuousInputNodeHistory
+    input_value::Vector{Union{Real,Missing}} = []
+    value_prediction_error::Vector{Union{Real,Missing}} = []
+    precision_prediction_error::Vector{Union{Real,Missing}} = []
+    prediction_mean::Vector{Union{Real,Missing}} = []
+    prediction_precision::Vector{Union{Real,Missing}} = []
 end
 
+"""
+"""
 Base.@kwdef mutable struct ContinuousInputNode <: AbstractContinuousInputNode
     name::String
     edges::ContinuousInputNodeEdges = ContinuousInputNodeEdges()
@@ -288,30 +289,30 @@ end
 """
  Configure parameters of binary state node
 """
-Base.@kwdef mutable struct BinaryStateNodeParameters{T<:Real}
-    coupling_strengths::Dict{String,T} = Dict{String,Float64}()
+Base.@kwdef mutable struct BinaryStateNodeParameters
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
 end
 
 """
 Overview of the states of the binary state node
 """
-Base.@kwdef mutable struct BinaryStateNodeState{T<:Real}
-    posterior_mean::Union{T1,Missing} = missing
-    posterior_precision::Union{T,Missing} = missing
-    value_prediction_error::Union{T,Missing} = missing
-    prediction_mean::Union{T,Missing} = missing
-    prediction_precision::Union{T,Missing} = missing
+Base.@kwdef mutable struct BinaryStateNodeState
+    posterior_mean::Union{Real,Missing} = missing
+    posterior_precision::Union{Real,Missing} = missing
+    value_prediction_error::Union{Real,Missing} = missing
+    prediction_mean::Union{Real,Missing} = missing
+    prediction_precision::Union{Real,Missing} = missing
 end
 
 """
 Overview of the history of the binary state node
 """
-Base.@kwdef mutable struct BinaryStateNodeHistory{T<:Real}
-    posterior_mean::Vector{Union{T,Missing}} = []
-    posterior_precision::Vector{Union{T,Missing}} = []
-    value_prediction_error::Vector{Union{T,Missing}} = []
-    prediction_mean::Vector{Union{T,Missing}} = []
-    prediction_precision::Vector{Union{T,Missing}} = []
+Base.@kwdef mutable struct BinaryStateNodeHistory
+    posterior_mean::Vector{Union{Real,Missing}} = []
+    posterior_precision::Vector{Union{Real,Missing}} = []
+    value_prediction_error::Vector{Union{Real,Missing}} = []
+    prediction_mean::Vector{Union{Real,Missing}} = []
+    prediction_precision::Vector{Union{Real,Missing}} = []
 end
 
 """
@@ -338,22 +339,22 @@ end
 """
 Configuration of parameters in binary input node. Default category mean set to [0,1]
 """
-Base.@kwdef mutable struct BinaryInputNodeParameters{T<:Real}
-    coupling_strengths::Dict{String,T} = Dict{String,Float64}()
+Base.@kwdef mutable struct BinaryInputNodeParameters
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
 end
 
 """
 Configuration of states of binary input node
 """
-Base.@kwdef mutable struct BinaryInputNodeState{T<:Real}
-    input_value::Union{T,Missing} = missing
+Base.@kwdef mutable struct BinaryInputNodeState
+    input_value::Union{Real,Missing} = missing
 end
 
 """
 Configuration of history of binary input node
 """
-Base.@kwdef mutable struct BinaryInputNodeHistory{T<:Real}
-    input_value::Vector{Union{T,Missing}} = [missing]
+Base.@kwdef mutable struct BinaryInputNodeHistory
+    input_value::Vector{Union{Real,Missing}} = [missing]
 end
 
 """
@@ -382,28 +383,28 @@ Base.@kwdef mutable struct CategoricalStateNodeEdges
         Vector{CategoricalInputNode}()
 end
 
-Base.@kwdef mutable struct CategoricalStateNodeParameters{T<:Real}
-    coupling_strengths::Dict{String,T} = Dict{String,Float64}()
+Base.@kwdef mutable struct CategoricalStateNodeParameters
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
 end
 
 """
 Configuration of states in categorical state node
 """
-Base.@kwdef mutable struct CategoricalStateNodeState{T<:Real}
-    posterior::Vector{Union{T,Missing}} = []
-    value_prediction_error::Vector{Union{T,Missing}} = []
-    prediction::Vector{Union{T,Missing}} = []
-    parent_predictions::Vector{Union{T,Missing}} = []
+Base.@kwdef mutable struct CategoricalStateNodeState
+    posterior::Vector{Union{Real,Missing}} = []
+    value_prediction_error::Vector{Union{Real,Missing}} = []
+    prediction::Vector{Union{Real,Missing}} = []
+    parent_predictions::Vector{Union{Real,Missing}} = []
 end
 
 """
 Configuration of history in categorical state node
 """
-Base.@kwdef mutable struct CategoricalStateNodeHistory{T<:Real}
-    posterior::Vector{Vector{Union{T,Missing}}} = []
-    value_prediction_error::Vector{Vector{Union{T,Missing}}} = []
-    prediction::Vector{Vector{Union{T,Missing}}} = []
-    parent_predictions::Vector{Vector{Union{T,Missing}}} = []
+Base.@kwdef mutable struct CategoricalStateNodeHistory
+    posterior::Vector{Vector{Union{Real,Missing}}} = []
+    value_prediction_error::Vector{Vector{Union{Real,Missing}}} = []
+    prediction::Vector{Vector{Union{Real,Missing}}} = []
+    parent_predictions::Vector{Vector{Union{Real,Missing}}} = []
 end
 
 """
@@ -428,22 +429,22 @@ Base.@kwdef mutable struct CategoricalInputNodeEdges
         Vector{CategoricalStateNode}()
 end
 
-Base.@kwdef mutable struct CategoricalInputNodeParameters{T<:Real}
-    coupling_strengths::Dict{String,T} = Dict{String,Float64}()
+Base.@kwdef mutable struct CategoricalInputNodeParameters
+    coupling_strengths::Dict{String,Real} = Dict{String,Real}()
 end
 
 """
 Configuration of states of categorical input node
 """
-Base.@kwdef mutable struct CategoricalInputNodeState{T<:Real}
-    input_value::Union{T,Missing} = missing
+Base.@kwdef mutable struct CategoricalInputNodeState
+    input_value::Union{Real,Missing} = missing
 end
 
 """
 History of categorical input node
 """
-Base.@kwdef mutable struct CategoricalInputNodeHistory{T<:Real}
-    input_value::Vector{Union{T,Missing}} = [missing]
+Base.@kwdef mutable struct CategoricalInputNodeHistory
+    input_value::Vector{Union{Real,Missing}} = [missing]
 end
 
 """
