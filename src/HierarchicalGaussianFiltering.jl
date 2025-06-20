bb0a4bb9f7099066565532b803fa831ee47e9aa0
Peter Thestrup Waade <peter@waade.net>
2025-06-20 17:57:23 +0200

    updated to ActionModels 0.7

--- src/HierarchicalGaussianFiltering.jl
+++ src/HierarchicalGaussianFiltering.jl
@@ -1,7 +1,7 @@
 module HierarchicalGaussianFiltering
 
 #Load packages
-using ActionModels, Distributions, RecipesBase
+using ActionModels, RecipesBase
 
 #Export functions
 export init_node, init_hgf, premade_hgf, check_hgf, update_hgf!
