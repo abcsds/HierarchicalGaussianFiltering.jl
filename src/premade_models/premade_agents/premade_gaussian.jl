585dfd230741dc95b4f793db51f5a9f51ce06c07
Peter Thestrup Waade <peter@waade.net>
2025-06-21 08:22:24 +0200

    updated premade_gaussian

--- src/premade_models/premade_agents/premade_gaussian.jl
+++ src/premade_models/premade_agents/premade_gaussian.jl
@@ -1,100 +1,65 @@
-"""
-    hgf_gaussian(agent::Agent, input)
+export HGFGaussian
 
-Action model which reports a given HGF state with Gaussian noise.
-
-In addition to the HGF substruct, the following must be present in the agent:
-Parameters: "gaussian_action_precision"
-Settings: "target_state"
-"""
-function hgf_gaussian(agent::Agent, input)
-
-    #Extract HGF, settings and parameters
-    hgf = agent.substruct
-    target_state = agent.settings["target_state"]
-    action_noise = agent.parameters["action_noise"]
-
-    #Update the HGF
-    update_hgf!(agent.substruct, input)
-
-    #Extract specified belief state
-    action_mean = get_states(hgf, target_state)
-
-    #If the gaussian mean becomes a NaN
-    if isnan(action_mean)
-        #Throw an error that will reject samples when fitted
-        throw(
-            RejectParameters(
-                "With these parameters and inputs, the mean of the gaussian action became $action_mean, which is invalid. Try other parameter settings",
-            ),
-        )
-    end
-
-    #Create normal distribution with mean of the target value and a standard deviation from parameters
-    distribution = Distributions.Normal(action_mean, action_noise)
-
-    #Return the action distribution
-    return distribution
+Base.@kwdef struct HGFGaussian <: AbstractPremadeModel
+    action_noise::Float64 = 1.0
+    target_state::Symbol = "x_posterior_mean"
+    HGF::Union{HGF,String} = "continuous_2level"
 end
 
+function ActionModel(config::HGFGaussian)
 
-"""
-    premade_hgf_gaussian(config::Dict)
+    #Extract hgf
+    if config.HGF isa String
+        #If the HGF is a string, we assume it is a name of a premade HGF
+        hgf = premade_hgf(config.HGF)
+    else
+        hgf = config.HGF
+    end
+    #Extract target state
+    target_state = config.target_state
 
-Create an agent suitable for the HGF Gaussian action model.
+    #Create action model function
+    am_function = function hgf_gaussian(
+        attributes::ModelAttributes,
+        hgf_observation::R,
+    ) where {R<:Real}
 
-# Config defaults:
- - "HGF": "continuous_2level"
- - "gaussian_action_precision": 1
- - "target_state": ("x", "posterior_mean")
-"""
-function premade_hgf_gaussian(config::Dict)
+        #Extract HGF, settings and parameters
+        hgf = attributes.submodel
 
-    ## Combine defaults and user settings
+        β = load_parameters(attributes).action_noise
 
-    #Default parameters and settings
-    defaults = Dict(
-        "action_noise" => 1,
-        "target_state" => ("x", "posterior_mean"),
-        "HGF" => "continuous_2level",
-    )
+        #Update the HGF
+        update_hgf!(hgf, hgf_observation)
 
-    #If there is no HGF in the user-set parameters
-    if !("HGF" in keys(config))
-        HGF_name = defaults["HGF"]
-        #Make a default HGF
-        config["HGF"] = premade_hgf(HGF_name)
-        #And warn them
-        @warn "an HGF was not set by the user. Using the default: a $HGF_name HGF with default settings"
+        #Extract specified belief state
+        μ = get_states(hgf, target_state)
+
+        #If the gaussian mean becomes a NaN
+        if isnan(μ)
+            #Throw an error that will reject samples when fitted
+            throw(
+                RejectParameters(
+                    "With these parameters and inputs, the mean of the gaussian action became $μ, which is invalid. Try other parameter settings",
+                ),
+            )
+        end
+
+        #Create normal distribution with mean of the target value and a standard deviation from parameters
+        return Normal(μ, β)
     end
 
-    #Warn the user about used defaults and misspecified keys
-    warn_premade_defaults(defaults, config)
+    parameters = (action_noise = Parameter(config.action_noise),)
 
-    #Merge to overwrite defaults
-    config = merge(defaults, config)
+    observations = (; hgf_observation = Observation(Real))
 
+    actions = (; report = Action(Normal),)
 
-    ## Create agent 
-    #Set the action model
-    action_model = hgf_gaussian
-
-    #Set the HGF
-    hgf = config["HGF"]
-
-    #Set parameters
-    parameters = Dict("action_noise" => config["action_noise"])
-    #Set states
-    states = Dict()
-    #Set settings
-    settings = Dict("target_state" => config["target_state"])
-
-    #Create the agent
-    return init_agent(
-        action_model;
-        substruct = hgf,
+    return ActionModel(
+        am_function,
         parameters = parameters,
-        states = states,
-        settings = settings,
+        observations = observations,
+        actions = actions,
     )
+
 end
