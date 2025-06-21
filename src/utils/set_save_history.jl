22f15629955d1132b3e8d4f0c9f36807b27d8a51
Peter Thestrup Waade <peter@waade.net>
2025-06-21 10:45:58 +0200

    added save history functionality

---
+++ src/utils/set_save_history.jl
@@ -0,0 +1,8 @@
+#To set save history
+function set_save_history!(hgf::HGF, save_history::Bool)
+    hgf.save_history = save_history
+end
+
+function set_save_history!(agent::Agent{HGF}, save_history::Bool)
+    agent.model_attributes.submodel.save_history = save_history
+end
