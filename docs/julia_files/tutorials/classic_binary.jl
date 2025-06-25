87aa2f49ba7a128e39fc51c8323f5e073022f3e3
Peter Thestrup Waade <peter@waade.net>
2025-06-25 09:19:23 +0200

    updated tutorial code

--- docs/julia_files/tutorials/classic_binary.jl
+++ docs/julia_files/tutorials/classic_binary.jl
@@ -7,9 +7,7 @@ using ActionModels
 using HierarchicalGaussianFiltering
 using CSV
 using DataFrames
-using Plots
 using StatsPlots
-using Distributions
 
 # Get the path for the HGF superfolder
 hgf_path = dirname(dirname(pathof(HierarchicalGaussianFiltering)))
@@ -34,66 +32,39 @@ hgf_parameters = Dict(
 hgf = premade_hgf("binary_3level", hgf_parameters, verbose = false);
 
 # Create an agent
-agent_parameters = Dict("action_noise" => 0.2);
-agent = premade_agent("hgf_unit_square_sigmoid", hgf, agent_parameters, verbose = false);
+action_model = ActionModel(HGFSigmoid(; HGF = hgf, action_noise = 0.2))
+agent = init_agent(action_model);
 
 # Evolve agent and save actions
 actions = simulate!(agent, inputs);
 
 # Plot the trajectory of the agent
-plot_trajectory(agent, ("u", "input_value"))
-plot_trajectory!(agent, ("xbin", "prediction"))
+plot(agent, ("u", "input_value"))
+plot!(agent, ("xbin", "prediction"))
 
 # -
 
-plot_trajectory(agent, ("xprob", "posterior"))
-plot_trajectory(agent, ("xvol", "posterior"))
+plot(agent, ("xprob", "posterior"))
+plot(agent, ("xvol", "posterior"))
 
-# Set fixed parameters
-fixed_parameters = Dict(
-    "action_noise" => 0.2,
-    ("xprob", "initial_mean") => 0,
-    ("xprob", "initial_precision") => 1,
-    ("xvol", "initial_mean") => 1,
-    ("xvol", "initial_precision") => 1,
-    ("xbin", "xprob", "coupling_strength") => 1.0,
-    ("xprob", "xvol", "coupling_strength") => 1.0,
-    ("xvol", "volatility") => -6.0,
-);
 
 # Set priors for parameter recovery
-param_priors = Dict(("xprob", "volatility") => Normal(-3.0, 0.5));
-#-
-# # Prior predictive plot
-# plot_predictive_simulation(
-#     param_priors,
-#     agent,
-#     inputs,
-#     ("xbin", "prediction_mean"),
-#     n_simulations = 100,
-# )
+prior = (xprob_volatility = Normal(-3.0, 0.5);)
+
 #-
 # Get the actions from the MATLAB tutorial
 actions = CSV.read(data_path * "classic_binary_actions.csv", DataFrame)[!, 1];
 #-
 # Fit the actions
 #Create model
-model = create_model(agent, param_priors, inputs, actions)
+model = create_model(action_model, prior, inputs, actions, check_parameter_rejections = true)
 
-#Fit single chain with 10 iterations
-fitted_model = fit_model(model; n_iterations = 10, n_chains = 1)
+#Fit model
+posterior_chains = sample_posterior!(
+    model,
+    n_samples = 200,
+    n_chains = 2,
+)
 #-
 #Plot the chains
-plot(fitted_model.chains)
-#-
-# Plot the posterior
-# plot_parameter_distribution(fitted_model, param_priors)
-#-
-# Posterior predictive plot
-# plot_predictive_simulation(
-#     fitted_model,
-#     agent,
-#     inputs,
-#     ("xbin", "prediction_mean"),
-#     n_simulations = 3,
-# )
+plot(posterior_chains)
